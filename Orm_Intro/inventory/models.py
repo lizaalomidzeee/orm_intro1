2130e227f2bc0831626ccc1065ef2bdb28a24cdd
Lukaandriadze <147325836+Lukaandriadze@users.noreply.github.com>
2024-11-16 12:23:36 -0800

    Update models.py

--- Orm_Intro/inventory/models.py
+++ Orm_Intro/inventory/models.py
@@ -2,10 +2,10 @@
 from django.db import models
 
 class InventoryItem(models.Model):
-    name = models.CharField(max_length=100)  # Product name (text field)
-    description = models.TextField()  # Product description (text field)
-    price = models.DecimalField(max_digits=10, decimal_places=2)  # Price (decimal field)
-    stock = models.IntegerField()  # Stock (integer field)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2) 
+    stock = models.IntegerField()
 
     def __str__(self):
         return self.name
